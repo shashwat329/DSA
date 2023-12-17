f4831a568034ac17a4dd90976a82fe7623fc0392
shashwat329 <shashwatrajwanshi41@gmail.com>
2023-12-17 22:56:07 +0530

    revision

--- udemy/mergesortrevision/mergesort.cpp
+++ udemy/mergesortrevision/mergesort.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
 using namespace std;
-void mergesort(int arr[],int size){
-    int mid=size/2;
-    
+void mergeArray(int arr1[],int arr2[],int n1,int n2){
+    int arr[n1+n2];
+    int * p1,*p2;
+}
+void mergesort(int arr[],int size1){
+    int mid=size1/2;
+
 }
 int main(){
-
+    int arr1[]={1,2,3};
+    int n1=3;
+    int arr2[]={13,4,2,6,9,10};
+    int n2=6;
+    // int ans = mergeArray(arr1,arr2);
+    // cout<<ans<<endl;
     return 0;
 }
