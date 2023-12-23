661f040f4dd09034c4cba706df8cfdc427417854
shashwat329 <shashwatrajwanshi41@gmail.com>
2023-12-24 00:02:16 +0530

    queue

--- udemy/queue/primitive_queue.cpp
+++ udemy/queue/primitive_queue.cpp
@@ -9,20 +9,31 @@ class queue{
     int rear=-1;
     int size =0;
     void push(int a){
-        if(front<=rear){
-        if(front ==-1){
-            front++;
-        }
-        rear++;
+   if(size==max_size){
+    //  queue is full
+    cout<<"queue is full so element cant be pushed"<<endl;
+   }
+   else{
+    if(size!=0){
         size++;
+        rear++;
         arr[rear]=a;
     }
+    if(size==0||front==-1){
+        size++;
+        front++;
+        rear++;
+        arr[rear]=a;
     }
+   }
+   
+   }
     void pop(){
        if(size==0){
         cout<<"deletion cant be performed!"<<endl;
        }
        else{
+         arr[front]=-1;
         front++;
         size--;
        }
@@ -36,5 +47,22 @@ class queue{
 };
 
 int main(){
+    queue q1;
+    q1.push(1);
+    q1.push(2);
+    q1.push(3);
+    q1.push(4);
+    q1.push(5);
+    q1.push(6);
+    q1.push(7);
+    q1.push(8);
+    q1.push(9);
+    q1.push(10);
+    q1.push(11);
+
+
+
+    q1.display();
+    cout<<q1.size;
     return 0;
 }
