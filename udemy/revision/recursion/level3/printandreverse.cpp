9c855cfc2fc47af6eb45abd5dd3f6c39f391a575
shashwat329 <shashwatrajwanshi41@gmail.com>
2024-01-29 22:42:18 +0530

    level3

---
+++ udemy/revision/recursion/level3/printandreverse.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void print(int arr[],int size){
+    if(size==0) return ;
+    cout<<arr[0]<<" ";
+    print(arr+1,size-1);
+
+}
+int main(){
+    int arr[]={1,3,12,16,31,32};
+    print(arr,6);
+    return 0;
+}
