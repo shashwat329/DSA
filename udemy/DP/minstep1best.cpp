9ff7d9edd0bf6cf7505d0e3dfc3e4780a694a46d
shashwat41 <shashwatrajwanshi41@gmail.com>
2024-04-14 23:24:20 +0530

    DP

--- udemy/DP/minstep1best.cpp
+++ udemy/DP/minstep1best.cpp
@@ -3,23 +3,24 @@ using namespace std;
 int minstep_best(int n){
     int *dp = new int[n+1];
     dp[0]=0;
-    dp[1]=1;
-    for(int i =1;i<=n;i++){
+    dp[1]=0;
+    for(int i =2;i<=n;i++){
         int x = dp[i-1];
         int y,z;
-        y,z = INT_MAX;
+        y=z = INT_MAX;
         if(i%2==0){
             y = dp[i/2];
         }
-        if(n%3==0){
+        if(i%3==0){
             z= dp[i/3];
         }
+        dp[i]=min(x,min(y,z))+1;
     }
     return dp[n];
 }
 int main(){
     int n;
     cin>>n;
-    cout<<minstep_best(10)<<endl;
+    cout<<minstep_best(n)<<endl;
     return 0;
 }
